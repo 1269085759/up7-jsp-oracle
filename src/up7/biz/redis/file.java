240ebb81fbc802af886f452a0217310ac4ff9ecb
qwl <qwl@ncmem.com>
2017-05-05 15:58:51 +0800

    完善文件夹，写数据库逻辑，增加rootSign

--- src/up7/biz/redis/file.java
+++ src/up7/biz/redis/file.java
@@ -44,6 +44,7 @@ public class file
 		if(j.exists(f.idSign)) return;
 		
 		j.hset(f.idSign, "fdTask", f.f_fdTask==true?"true":"false");
+		j.hset(f.idSign, "rootSign", f.rootSign);
 		j.hset(f.idSign, "pathLoc", f.pathLoc);
 		j.hset(f.idSign, "pathSvr", f.pathSvr);
 		j.hset(f.idSign, "nameLoc", f.nameLoc);
